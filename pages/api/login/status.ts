19e11759d4c84c37c305c942ac62f37eeec10b62
tohsaka888 <156132264@qq.com>
2022-09-05 10:53:40 +0800

    ✨ feat(api): 管理员登录状态

---
+++ pages/api/login/status.ts
@@ -0,0 +1,81 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-09-05 10:50:30
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-09-05 10:50:31
+ * @Description: 请填写简介
+ */
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import Cors from 'cors'
+import { runMiddleware } from '@utils/runMiddleware'
+import jwt from 'jsonwebtoken'
+
+/**
+ * @openapi
+ * /api/login/status:
+ *   post:
+ *     description: 获取登录状态
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               token:
+ *                 type: string
+ *                 description: 登录凭据
+ *             required:
+ *               - token
+ *             example:
+ *               token: xxxxxxxxx
+ *     responses:
+ *       200:
+ *         description: 返回登录状态
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success: 
+ *                   type: boolean
+ *                 isLogin: 
+ *                   type: boolean
+ *                 username:
+ *                   type: string
+ *         example:
+ *           success: true
+ *           isLogin: true
+ *           username: tohsaka888
+ *               
+ */
+
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+})
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  try {
+
+    // Run the middleware
+    await runMiddleware(req, res, cors)
+
+    const body: { token: string } = req.body
+    
+    const result = jwt.verify(body.token, process.env.secret as string)
+
+    res.status(200).json({ success: true, isLogin: result ? true : false, result })
+
+  } catch (error: any) {
+    res.status(200).send({
+      success: false,
+      error: (error as Error).message
+    })
+  }
+}
