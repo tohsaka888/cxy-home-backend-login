5c42cfd4976e8caeb821695b33b67d95fcc6ff95
tohsaka888 <156132264@qq.com>
2022-09-05 11:51:11 +0800

    🐞 fix: 登录token不包含密码

--- utils/generateAccessToken.ts
+++ utils/generateAccessToken.ts
@@ -2,7 +2,7 @@
  * @Author: tohsaka888
  * @Date: 2022-09-05 10:40:17
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-09-05 10:45:40
+ * @LastEditTime: 2022-09-05 11:07:37
  * @Description: 请填写简介
  */
 
@@ -15,5 +15,6 @@ type Props = {
 }
 
 export function generateAccessToken({ admin, id }: Props) {
+  delete admin.adminPass
   return jwt.sign({ ...admin, id }, process.env.secret || '', { expiresIn: '30d' });
 }
